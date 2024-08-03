8ea3e9e432d57d0dc4ee90223ea281af90d0f297
Nobody <bunnza2020@gmail.com>
2024-08-03 14:49:34 -0500

    Ability to Draw Solid Rectangle

--- main.py
+++ main.py
@@ -8,23 +8,27 @@ from kivy.graphics import Color, Line, Rectangle
 
 class MainInterface(GridLayout):
     Place_Shape = ""
-    # def Test(self):
-    #     print(self.ids.Red_Slider.value)
     
     def Set_Shape(self,Shape:str):
         self.Place_Shape = Shape
     
-    def on_touch_down(self, touch):
-        #Where I want to check if click is in canvas
-        print("Mouse Down", touch)
-        bottom_left = self.ids.Canvas_Box.pos
-        top_left = (self.ids.Canvas_Box.x, self.ids.Canvas_Box.y + self.ids.Canvas_Box.height)
-        bottom_right = (self.ids.Canvas_Box.x + self.ids.Canvas_Box.width, self.ids.Canvas_Box.y)
-        top_right = (self.ids.Canvas_Box.x + self.ids.Canvas_Box.width, self.ids.Canvas_Box.y + self.ids.Canvas_Box.height)
-
-        with self.ids.Canvas_Box.canvas:
-            Color(1, 0, 0, 1)  
-            Line(points=[*bottom_left, *top_left, *top_right, *bottom_right, *bottom_left])
+    def Draw_Shape(self,pos):
+        W = int(self.ids.Width_Slider.value)
+        H = int(self.ids.Height_Slider.value)
+        if self.Place_Shape == "Rectangle":Rectangle(pos=pos, size=(W,H))
+    
+    def on_touch_up(self, touch):
+        Touch_x, Touch_y = touch.pos
+        if self.ids.Canvas_Box.x < Touch_x < (self.ids.Canvas_Box.x + self.ids.Canvas_Box.width) and self.ids.Canvas_Box.y < Touch_y < (self.ids.Canvas_Box.y + self.ids.Canvas_Box.height):
+            with self.ids.Canvas_Box.canvas:
+                R = int(self.ids.Red_Slider.value)/100
+                G = int(self.ids.Green_Slider.value)/100
+                B = int(self.ids.Blue_Slider.value)/100
+                A = int(self.ids.Alpha_Slider.value)/100
+                Color(R, G, B, A)  
+                if self.ids.Hollow_Checker.active:
+                    pass
+                else: self.Draw_Shape(touch.pos)
 
 
 class CanvasApp(App):
