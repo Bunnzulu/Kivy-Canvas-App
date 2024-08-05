2364b69267c914461360122a2e3cd27b81e69630
Nobody <bunnza2020@gmail.com>
2024-08-05 03:44:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class MainInterface(GridLayout):
     Triangle_Placements = [(),(),()]
     Quad_Placements = [(),(),(),()]
     Shapes = []
+    Shapes_points = []
     Old_Window_size = []
     Old_Color = None
 
@@ -28,6 +29,7 @@ class MainInterface(GridLayout):
         center = (pos[0] - (W/2),pos[1] - (H/2))
         THICC = int(self.ids.Thickness_Slider.value)
         Shape = None
+        Shape_points = None
         if self.Place_Shape == "Rectangle":Shape = Rectangle(pos=center, size=(W,H))
         elif self.Place_Shape == "Ellipse":Shape = Ellipse(pos=center, size=(W, H))
         elif self.Place_Shape == "Circle":Shape = Ellipse(pos=(pos[0] - RADIUS, pos[1] - RADIUS), size=(RADIUS * 2, RADIUS * 2), angle_start=0, angle_end=360)
@@ -35,7 +37,7 @@ class MainInterface(GridLayout):
             if self.Line_Placements[0] == ():self.Line_Placements[0] = pos
             elif self.Line_Placements[1] == ():
                 self.Line_Placements[1] = pos
-                Shape = Line(points=(self.Line_Placements[0][0],self.Line_Placements[0][1],self.Line_Placements[1][0],self.Line_Placements[1][1]),width=THICC)
+                Shape_points = Line(points=(self.Line_Placements[0][0],self.Line_Placements[0][1],self.Line_Placements[1][0],self.Line_Placements[1][1]),width=THICC)
                 self.Line_Placements = [(),()]
         elif self.Place_Shape == "Quad":
             if self.Quad_Placements[0] == ():self.Quad_Placements[0] = pos
@@ -43,16 +45,17 @@ class MainInterface(GridLayout):
             elif self.Quad_Placements[2] == ():self.Quad_Placements[2] = pos
             elif self.Quad_Placements[3] == ():
                 self.Quad_Placements[3] = pos
-                Shape = Quad(points=(*self.Quad_Placements[0],*self.Quad_Placements[1],*self.Quad_Placements[2],*self.Quad_Placements[3]))
+                Shape_points = Quad(points=(*self.Quad_Placements[0],*self.Quad_Placements[1],*self.Quad_Placements[2],*self.Quad_Placements[3]))
                 self.Quad_Placements = [(),(),(),()]
         elif self.Place_Shape == "Triangle":
             if self.Triangle_Placements[0] == ():self.Triangle_Placements[0] = pos
             elif self.Triangle_Placements[1] == ():self.Triangle_Placements[1] = pos
             elif self.Triangle_Placements[2] == ():
                 self.Triangle_Placements[2] = pos
-                Shape = Triangle(points=(*self.Triangle_Placements[0],*self.Triangle_Placements[1],*self.Triangle_Placements[2]))
+                Shape_points = Triangle(points=(*self.Triangle_Placements[0],*self.Triangle_Placements[1],*self.Triangle_Placements[2]))
                 self.Triangle_Placements = [(),(),()]
         if Shape != None:self.Shapes.append((Shape,self.Old_Color))
+        if Shape_points != None: self.Shapes_points.append((Shape_points,self.Old_Color))
 
     def Draw_Hollow_Shape(self,pos):
         W = int(self.ids.Width_Slider.value)
@@ -61,14 +64,17 @@ class MainInterface(GridLayout):
         THICC = int(self.ids.Thickness_Slider.value)
         center = (pos[0] - (W/2),pos[1] - (H/2))
         Shape = None
+        Shape_points = None
         if self.Place_Shape == "Rectangle":Shape = Line(rectangle=(center[0],center[1],W,H),width=THICC)
         elif self.Place_Shape == "Ellipse":Shape = Line(ellipse=(center[0], center[1], W,H),width=THICC)
-        elif self.Place_Shape == "Circle":Shape = Line(circle=(pos[0], pos[1], RADIUS),width=THICC)
+        elif self.Place_Shape == "Circle":
+            Shape = Line(circle=(pos[0], pos[1], RADIUS),width=THICC)
+            print(Shape.points)
         elif self.Place_Shape == "Line":
             if self.Line_Placements[0] == ():self.Line_Placements[0] = pos
             elif self.Line_Placements[1] == ():
                 self.Line_Placements[1] = pos
-                Shape= Line(points=(self.Line_Placements[0][0],self.Line_Placements[0][1],self.Line_Placements[1][0],self.Line_Placements[1][1]),width=THICC)
+                Shape_points= Line(points=(self.Line_Placements[0][0],self.Line_Placements[0][1],self.Line_Placements[1][0],self.Line_Placements[1][1]),width=THICC)
                 self.Line_Placements = [(),()]
         elif self.Place_Shape == "Quad":
             if self.Quad_Placements[0] == ():self.Quad_Placements[0] = pos
@@ -76,16 +82,17 @@ class MainInterface(GridLayout):
             elif self.Quad_Placements[2] == ():self.Quad_Placements[2] = pos
             elif self.Quad_Placements[3] == ():
                 self.Quad_Placements[3] = pos
-                Shape = Line(points=[*self.Quad_Placements[0],*self.Quad_Placements[1],*self.Quad_Placements[2],*self.Quad_Placements[3]],close=True,width=THICC)
+                Shape_points = Line(points=[*self.Quad_Placements[0],*self.Quad_Placements[1],*self.Quad_Placements[2],*self.Quad_Placements[3]],close=True,width=THICC)
                 self.Quad_Placements = [(),(),(),()]
         elif self.Place_Shape == "Triangle":
             if self.Triangle_Placements[0] == ():self.Triangle_Placements[0] = pos
             elif self.Triangle_Placements[1] == ():self.Triangle_Placements[1] = pos
             elif self.Triangle_Placements[2] == ():
                 self.Triangle_Placements[2] = pos
-                Shape = Line(points=[*self.Triangle_Placements[0],*self.Triangle_Placements[1],*self.Triangle_Placements[2]],close=True,width=THICC)
+                Shape_points = Line(points=[*self.Triangle_Placements[0],*self.Triangle_Placements[1],*self.Triangle_Placements[2]],close=True,width=THICC)
                 self.Triangle_Placements = [(),(),()]
         if Shape != None:self.Shapes.append(Shape)
+        if Shape_points != None: self.Shapes_points.append((Shape_points,self.Old_Color))
         
     def Clear_Canvas(self):
         self.ids.Canvas_Box.canvas.clear()
@@ -93,6 +100,10 @@ class MainInterface(GridLayout):
   
     def Resize_Drawings(self, *args):
         self.ids.Canvas_Box.canvas.clear()
+        self.Resize_Shapes()
+        self.Resize_Shapes_points()
+    
+    def Resize_Shapes(self):
         for i in range(len(self.Shapes)):
             ratio_x = self.Shapes[i][0].pos[0]/self.Old_Window_size[0]
             ratio_y = self.Shapes[i][0].pos[1]/self.Old_Window_size[1]
@@ -102,7 +113,21 @@ class MainInterface(GridLayout):
             self.ids.Canvas_Box.canvas.add(self.Shapes[i][1])
             self.ids.Canvas_Box.canvas.add(self.Shapes[i][0])
         self.Old_Window_size = [self.width,self.height]
-        print(self.Old_Window_size)
+
+    def Resize_Shapes_points(self):
+        for i in range(len(self.Shapes_points)):
+            for point in range(len(self.Shapes_points[i][0].points)):
+                if point%2 == 0:
+                    ratio = self.Shapes_points[i][0].points[point]/self.Old_Window_size[0]
+                    new_point = self.width * ratio
+                else:
+                    ratio = self.Shapes_points[i][0].points[point]/self.Old_Window_size[1]
+                    new_point = self.height * ratio
+                self.Shapes_points[i][0].points[point] = new_point
+            self.ids.Canvas_Box.canvas.add(self.Shapes_points[i][1])
+            self.ids.Canvas_Box.canvas.add(self.Shapes_points[i][0])
+        self.Old_Window_size = [self.width,self.height]
+            
 
     def on_touch_up(self, touch):
         Touch_x, Touch_y = touch.pos
